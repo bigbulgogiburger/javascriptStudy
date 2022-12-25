7c72b74918bc7f7de1224da860691599146abbbe
dhpyun <dohoon321@gmail.com>
2022-12-26 00:18:41 +0900

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('#d-day-container');
 const messageContainer = document.querySelector('.d-day-message');
-
+const intervalIdArr = [];
 container.style.display='none';
 messageContainer.textContent='D-day를 입력해 주세요.';
 
@@ -16,15 +16,26 @@ const dateFormMaker = function(){
     
 
 const countMaker = function(){
+    console.log('반복실행중');
     const targetDateInput = dateFormMaker();
     const nowDate = new Date();
     const targetDate = new Date(targetDateInput).setHours(0,0,0,0);
     const remaining = (targetDate - nowDate)/ 1000;
     if(remaining <= 0){
+        container.style.display='none';
         messageContainer.innerHTML = '<h3>타이머가 종료되었습니다.</h3>'
+        messageContainer.style.display='flex';
+        setClearInterval();
+        return;
     }else if(isNaN(remaining)){
+        container.style.display='none';
         messageContainer.innerHTML = '<h3>유효한 시간대가 아닙니다.</h3>'
+        messageContainer.style.display='flex';
+        setClearInterval();
+        return;
     }
+
+
 
     const remainingObj ={
         remainingDate : Math.floor(remaining /3600 /24 ),
@@ -33,20 +44,47 @@ const countMaker = function(){
         remainingSec : Math.floor(remaining)%60
     };
 
-    const documentObj ={
-        days : document.getElementById('days'),
-        hours : document.getElementById('hours'),
-        min : document.getElementById('min'),
-        sec : document.getElementById('sec')
-    }
+    // const documentObj ={
+    //     days : document.getElementById('days'),
+    //     hours : document.getElementById('hours'),
+    //     min : document.getElementById('min'),
+    //     sec : document.getElementById('sec')
+    // }
 
-    documentObj['days'].textContent = remainingObj['remainingDate'];
-    documentObj['hours'].textContent = remainingObj['remainingHours'];
-    documentObj['min'].textContent = remainingObj['remainingMin'];
-    documentObj['sec'].textContent = remainingObj['remainingSec'];
-
-    if(container.style.display == 'none'){
-        container.style.display='flex'
+    // if(container.style.display == 'none'){
+    //     container.style.display='flex'
+    // }
+    
+    const documentArr= ['days','hours','min','sec'];
+    const timeKeys = Object.keys(remainingObj);
+    let a =0;
+    for(let tag of documentArr){
+        document.getElementById(tag).textContent=remainingObj[timeKeys[a]];
+        a++;
     }
-    console.log(documentObj['days'],documentObj['hours'],documentObj['min'],documentObj['sec'] )
+    
+    
+    // const docKeys = Object.keys(documentObj);
+   
+    // let i =0
+    // for(let key in documentObj){
+    //     documentObj[key].textContent = remainingObj[timeKeys[i]];
+    //     i++;
+    // }
+}
+const starter = function(){
+    container.style.display ='flex';
+    messageContainer.style.display='none';
+    countMaker();
+    const intervalId = setInterval(countMaker,1000);
+    intervalIdArr.push(intervalId);
+};
+
+const setClearInterval =function(){
+    container.style.display='none';
+    messageContainer.style.display='flex';
+    messageContainer.textContent='D-day를 입력해 주세요.';
+    for(let i =0; i<intervalIdArr.length;i++){
+        clearInterval(intervalIdArr[i])
+    }
 }
